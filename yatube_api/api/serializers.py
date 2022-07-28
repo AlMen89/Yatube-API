b8681f9e59095e94c51b1369386851ab16f2269d
Александр Меньших <AlMen89@Yandex.ru>
2022-07-29 01:07:16 +0300

    final

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import User, Post, Group, Comment, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,11 +13,33 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
+    post = PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    queryset = User.objects.all()
+    user = SlugRelatedField(slug_field='username', read_only=True)
+    following = SlugRelatedField(slug_field='username', queryset=queryset)
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+
+    def validate_following(self, following):
+        if following == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя!')
+        return following
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,70 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.exceptions import ValidationError
+
+from posts.models import Post, Group, Comment, Follow
+
+from .serializers import (
+    PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    message = 'Изменение чужого контента запрещено!'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        get_object_or_404(Post, id=post_id)
+        return self.queryset.filter(post=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        following = serializer.validated_data['following']
+        finded_set = self.get_queryset().filter(
+            user=self.request.user, following=following)
+        if finded_set.exists():
+            raise ValidationError('Эта подписка уже существует!')
+        serializer.save(user=self.request.user)
